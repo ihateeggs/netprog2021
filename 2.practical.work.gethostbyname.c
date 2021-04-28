262aac38021aa6f34cefa1cd7e02eb677fdb578e
ihateeggs <daikia17@gmail.com>
2021-04-29 01:54:42 +0700

    Submit code labwork 2

---
+++ 2.practical.work.gethostbyname.c
@@ -0,0 +1,30 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    struct hostent *host;
+    if (argc < 2) {
+      char hostName[200];
+      printf("Enter hostname: ");
+      scanf("%s",hostName);
+      host = gethostbyname(hostName);
+    }
+    else {
+      host = gethostbyname(argv[1]);
+    }
+
+    if (host == NULL) {
+       printf("gethostbyname() failed\n");
+    } else {
+       printf("Resolved IP address of %s:\n", host->h_name);
+       int i=0;
+       while ( host -> h_addr_list[i] != NULL) {
+          printf( "\t-%s\n ", inet_ntoa( *( struct in_addr*)( host -> h_addr_list[i])));
+          i++;
+       }
+       printf("\n End!\n");
+    }
+
+}
