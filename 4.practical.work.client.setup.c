deb760d440cb4e2e5f8860fd8e998ce2a7f91ac8
ihateeggs <daikia17@gmail.com>
2021-05-04 11:43:24 +0700

    Submit labwork 4 for testing

---
+++ 4.practical.work.client.setup.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char const *agrv[]){
+    struct hostent *host;
+    if (argc < 2) {
+      char hostName[200];
+      printf("Enter hostname: ");
+      scanf("%s",hostName);
+      host = gethostbyname(hostName);
+    }
+    else {
+      host = gethostbyname(agrv[1]);
+    }
+
+    if (host == NULL) {
+       printf("gethostbyname() failed\n");
+    } 
+    else {
+       printf("Resolved IP address of %s:\n", host->h_name);
+       unsigned int i=0;
+       while ( host -> h_addr_list[i] != NULL) {
+          printf( "\t-%s\n ", inet_ntoa( *( struct in_addr*)( host -> h_addr_list[i])));
+          i++;
+    }
+    printf("\n");
+
+
+    int sockfd, clen, clientfd;
+    struct sockaddr_in saddr, caddr;
+    unsigned short port = 8784;
+    sockfd=socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+      printf("Error creating socket\n");
+      exit(1);
+    }
+
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    saddr.sin_port = htons(port);
+
+    if (bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
+      printf("Error binding\n");
+      exit(1);
+    }
+
+    if (listen(sockfd, 5) < 0) {
+      printf("Error listening\n");
+      exit(1);
+    }
+
+    clen=sizeof(caddr);
+    if ((clientfd=accept(sockfd, (struct sockaddr *) &caddr, &clen)) < 0) {
+      printf("Error accepting connection\n");
+      exit(1);
+    }
+
+    printf("Client accepted\n");
+
+    return 1;
+}
